45f219b13aae42cb3cdcdea109061b0bd8e47648
ShilpiMaurya <47273243+ShilpiMaurya@users.noreply.github.com>
2021-11-16 11:51:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GetStarted from "./components/GetStarted";
+import "./app.css";
+
+function App() {
+  return (
+    <>
+      <GetStarted />
+    </>
+  );
+}
+
+export default App;
